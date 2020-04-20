42d0e41a42fbda3e1ba28961a028c068b34dfbf6
Casandra <casandragrullon@pursuit.org>
2020-04-20 17:38:11 -0400

    started followedGroupsVC and created groupCell

--- findYourPeers/ViewControllers/FollowedGroupsController.swift
+++ findYourPeers/ViewControllers/FollowedGroupsController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class FollowedGroupsController: UIViewController {
 
+    private let followedGroupsView = FollowedGroupsView()
+    
+    override func loadView() {
+        view = followedGroupsView
+        followedGroupsView.backgroundColor = .systemBackground
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureCollectionView()
+        configureButtonActions()
     }
-
+    private func configureCollectionView() {
+        //followedGroupsView.collectionView.delegate = self
+        //followedGroupsView.collectionView.dataSource = self
+        //followedGroupsView.collectionView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellWithReuseIdentifier: <#T##String#>)
+    }
+    private func configureButtonActions() {
+        followedGroupsView.studyButton.addTarget(self, action: #selector(studyButtonPressed(_:)), for: .touchUpInside)
+        followedGroupsView.clubButton.addTarget(self, action: #selector(clubButtonPressed(_:)), for: .touchUpInside)
+        followedGroupsView.eventButton.addTarget(self, action: #selector(eventButtonPressed(_:)), for: .touchUpInside)
+    }
+    @objc private func studyButtonPressed(_ sender: UIButton) {
+        //filter by study groups
+    }
+    @objc private func clubButtonPressed(_ sender: UIButton) {
+        //filter by clubs
+    }
+    @objc private func eventButtonPressed(_ sender: UIButton) {
+        //filter by events
+    }
 
 }
 
