a3dca5256b88728caea7950d2f00c87cb1f10308
Casandra <casandragrullon@pursuit.org>
2020-04-21 11:31:16 -0400

    added views for marketplace, item detail, wishlist, and contact seller

---
+++ findYourPeers/Views/ItemDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  ItemDetailView.swift
+//  findYourPeers
+//
+//  Created by casandra grullon on 4/21/20.
+//  Copyright © 2020 Howard Chang. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
