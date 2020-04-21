d7f6d7251eb44d138182a71c2228dd023d961968
howardC56 <h2chang56@gmail.com>
2020-04-20 22:27:03 -0400

    basic groupDetailVC setup

--- findYourPeers/ViewControllers/GroupDetailViewController.swift
+++ findYourPeers/ViewControllers/GroupDetailViewController.swift
@@ -10,7 +10,35 @@ import UIKit
 
 class GroupDetailViewController: UIViewController {
 
-    let groupDetailView = GroupDetailView()
+    private var groupDetailView = GroupDetailView()
+    //private var group: Group!
+    
+//    private var posts = [Post]() {
+//        didSet {
+//            groupDetailView.tableView.reloadData()
+//        }
+//    }
+    
+    private var isFavorite = false {
+        didSet {
+            DispatchQueue.main.async {
+                if self.isFavorite == true {
+                    self.navigationItem.rightBarButtonItem?.image = UIImage(systemName: "star.fill")
+            } else {
+                    self.navigationItem.rightBarButtonItem?.image = UIImage(systemName: "star")
+            }
+            }
+        }
+    }
+    
+    lazy private var favorite: UIBarButtonItem = {
+                    [unowned self] in
+           return UIBarButtonItem(image: UIImage(systemName: "star"), style: .plain, target: self, action: #selector(configureFavorites(_:)))
+                    }()
+    
+    @objc private func configureFavorites(_ sender: UIBarButtonItem) {
+        
+    }
     
     override func loadView() {
         view = groupDetailView
@@ -18,10 +46,30 @@ class GroupDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //navigationItem.title = group.name.capitalized
+        navigationItem.rightBarButtonItem = favorite
+        groupDetailView.tableView.delegate = self
+        groupDetailView.tableView.dataSource = self
     }
     
 
 
+}
+
+extension GroupDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //posts.count
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GroupDetailViewCell", for: indexPath) as? GroupDetailViewCell else {
+//            fatalError("could not downcast to SearchViewTableViewCell")
+//        }
+//        let post = posts[indexPath.row]
+//        cell.configureCell(for: post)
+//        return cell
+    }
+    
+    
 }
