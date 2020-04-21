ccdfa92bff88fb984121cedd66842c46aa4e4280
howardC56 <h2chang56@gmail.com>
2020-04-21 14:12:00 -0400

    firebaseStuff

--- findYourPeers/FireBase/FireBaseDataService.swift
+++ findYourPeers/FireBase/FireBaseDataService.swift
@@ -39,6 +39,17 @@ final class DatabaseService {
                     }
         }
     }
+    
+    public func getGroups<T: Codable>(item: T.Type, completion: @escaping (Result<[T], Error>) -> ()) {
+        db.collection(DatabaseService.groupsCollection).getDocuments { (snapshot, error) in
+                if let error = error {
+                    completion(.failure(error))
+                } else if let snapshot = snapshot {
+                    let items = snapshot.documents.compactMap { try? $0.data(as: T.self) }
+                    completion(.success(items))
+        }
+    }
+    }
 
     public func addGroupToFavorties<T: Codable & Identifiable>(_ item: T, completion: @escaping (Result<Bool, Error>) -> ()) {
         let document = db.collection(DatabaseService.favoriteGroupsCollection).document(item.id as! String)
