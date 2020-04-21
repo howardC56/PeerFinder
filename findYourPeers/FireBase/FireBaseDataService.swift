4d2bdeac115fe8d00ac26be299f731df7be2d345
howardC56 <h2chang56@gmail.com>
2020-04-21 15:47:32 -0400

    firebase and showalert

--- findYourPeers/FireBase/FireBaseDataService.swift
+++ findYourPeers/FireBase/FireBaseDataService.swift
@@ -16,6 +16,8 @@ final class DatabaseService {
   static let groupsCollection = "groups"
   static let postsCollection = "posts"
   static let favoriteGroupsCollection = "favoriteGroups"
+  static let itemGroupCollection = "item"
+    static let favoriteItemCollection = "favoriteItems"
   private let db = Firestore.firestore()
   private init () {}
     
@@ -105,6 +107,83 @@ final class DatabaseService {
         } catch let error {
             completion(.failure(error))
         }
+    }
+    
+    public func createItem<T: Codable & Identifiable>(_ item: T, completion: @escaping (Result<Bool, Error>) -> ()) {
+        let document = db.collection(DatabaseService.itemGroupCollection).document(item.id as! String)
+        do {
+            try document.setData(from: item)
+            completion(.success(true))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    public func deleteItem<T: Codable & Identifiable>(_ item: T, completion: @escaping (Result<Bool, Error>) -> ()) {
+            db.collection(DatabaseService.itemGroupCollection).document(item.id as! String).delete { (error) in
+                    if let error = error {
+                        completion(.failure(error))
+                    } else {
+                        completion(.success(true))
+                    }
+        }
+    }
+    
+    public func getItems<T: Codable>(item: T.Type, completion: @escaping (Result<[T], Error>) -> ()) {
+        db.collection(DatabaseService.itemGroupCollection).getDocuments { (snapshot, error) in
+                if let error = error {
+                    completion(.failure(error))
+                } else if let snapshot = snapshot {
+                    let items = snapshot.documents.compactMap { try? $0.data(as: T.self) }
+                    completion(.success(items))
+        }
+    }
+    }
+
+    public func addItemToFavorties<T: Codable & Identifiable>(_ item: T, completion: @escaping (Result<Bool, Error>) -> ()) {
+        let document = db.collection(DatabaseService.favoriteItemCollection).document(item.id as! String)
+        do {
+            try document.setData(from: item)
+            completion(.success(true))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    public func deleteItemFromFavorites<T: Codable & Identifiable>(_ item: T, completion: @escaping (Result<Bool, Error>) -> ()) {
+            db.collection(DatabaseService.favoriteItemCollection).document(item.id as! String).delete { (error) in
+                    if let error = error {
+                        completion(.failure(error))
+                    } else {
+                        completion(.success(true))
+                    }
+        }
+    }
+    
+    public func itemIsFavorited<T: Codable & Identifiable>(_ item: T, completion: @escaping (Result<Bool, Error>) -> ()) {
+        db.collection(DatabaseService.favoriteItemCollection).whereField("id", isEqualTo: item.id as! String).getDocuments { (snapshot, error) in
+                if let error = error {
+                    completion(.failure(error))
+                } else if let snapshot = snapshot {
+                    let count = snapshot.documents.count
+                    if count > 0 {
+                        completion(.success(true))
+                    } else {
+                        completion(.success(false))
+                    }
+                }
+        }
+    }
+    
+    public func getitemFavoriteGroups<T: Codable>(item: T.Type, completion: @escaping (Result<[T], Error>) -> ()) {
+        db.collection(DatabaseService.favoriteItemCollection).getDocuments { (snapshot, error) in
+                if let error = error {
+                    completion(.failure(error))
+                } else if let snapshot = snapshot {
+                    let items = snapshot.documents.compactMap { try? $0.data(as: T.self) }
+                    completion(.success(items))
+        }
+    }
     }
 
     
--- findYourPeers/SupportingFiles/Extensions.swift
+++ findYourPeers/SupportingFiles/Extensions.swift
@@ -55,3 +55,12 @@ extension UIView {
     
 }
 
+extension UIViewController {
+    public func showAlert(title: String?, message: String?) {
+      let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+      let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+      alertController.addAction(okAction)
+      present(alertController, animated: true)
+    }
+}
+
