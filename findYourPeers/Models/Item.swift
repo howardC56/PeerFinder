b587c52d567c1592a8db5456ae4d66aceaedfcd9
Casandra <casandragrullon@pursuit.org>
2020-04-21 15:13:36 -0400

    added item model

---
+++ findYourPeers/Models/Item.swift
@@ -0,0 +1,21 @@
+//
+//  Item.swift
+//  findYourPeers
+//
+//  Created by casandra grullon on 4/21/20.
+//  Copyright © 2020 Howard Chang. All rights reserved.
+//
+
+import Foundation
+
+struct Item: Codable {
+    let id: String
+    let itemCondition: String
+    let itemDescription: String
+    let itemImages: [String]
+    let itemName: String
+    let itemPrice: Double
+    let sellerEmail: String
+    let sellerId: String
+    let sellerName: String
+}
