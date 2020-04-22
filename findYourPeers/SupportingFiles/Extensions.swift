01dc74f7508523036059a62f4b4cfe6cccbe3e22
howardC56 <h2chang56@gmail.com>
2020-04-22 18:48:20 -0400

    added shakeButton extension

--- findYourPeers/SupportingFiles/Extensions.swift
+++ findYourPeers/SupportingFiles/Extensions.swift
@@ -90,4 +90,21 @@ extension UITextField {
     }
 }
 
-
+extension UIButton {
+    
+    func shake() {
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.1
+        shake.repeatCount = 2
+        shake.autoreverses = true
+        
+        let fromPoint = CGPoint(x: center.x - 5, y: center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        let toPoint = CGPoint(x: center.x + 5, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        layer.add(shake, forKey: nil)
+    }
+    
+}
